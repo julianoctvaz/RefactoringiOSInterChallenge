3109cdc9d2fb89bb81bf393ae1bad91413ed7bd2
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-10-13 03:12:38 -0300

    (feature) Creates URLSection constants

---
+++ InterChallenge/Models/URLSection.swift
@@ -0,0 +1,14 @@
+//
+//  URLSection.swift
+//  InterChallenge
+//
+//  Created by Juliano Vaz on 13/10/21.
+//
+
+import Foundation
+
+enum URLSection {
+    static let domain = "https://jsonplaceholder.typicode.com/"
+    static let users = "users"
+
+}
