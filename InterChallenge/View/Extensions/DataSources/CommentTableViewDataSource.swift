10e7329ab3970c343bab2be3080a7e37143f3911
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-10-13 14:45:44 -0300

    (build) Creates CommentTableView datasource and delegate extensions

---
+++ InterChallenge/View/Extensions/DataSources/CommentTableViewDataSource.swift
@@ -0,0 +1,27 @@
+//
+//  CommentTableViewDataSource.swift
+//  InterChallenge
+//
+//  Created by Juliano Vaz on 13/10/21.
+//
+
+import UIKit
+
+extension CommentTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return comments.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TitleAndDescriptionCell", for: indexPath) as? TitleAndDescriptionTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let comment = comments[indexPath.row]
+        cell.selectionStyle = .none
+        cell.titleLabel.text = comment.name
+        cell.descriptionLabel.text = comment.body
+        
+        return cell
+    }
+}
--- InterChallenge/View/View Controllers/CommentTableViewController.swift
+++ InterChallenge/View/View Controllers/CommentTableViewController.swift
@@ -28,20 +28,7 @@ class CommentTableViewController: UITableViewController {
         })
    }
 
-   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return comments.count
-   }
     
-   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TitleAndDescriptionCell", for: indexPath) as? TitleAndDescriptionTableViewCell else {
-            return UITableViewCell()
-        }
-
-        let comment = comments[indexPath.row]
-        cell.selectionStyle = .none
-        cell.titleLabel.text = comment.name
-        cell.descriptionLabel.text = comment.body
-
-        return cell
-    }
 }
+
+
