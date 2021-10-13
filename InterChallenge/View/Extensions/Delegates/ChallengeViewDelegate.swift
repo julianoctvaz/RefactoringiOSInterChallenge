91859d59dbc6743f02f8f9ae71b755da4573bbc8
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-10-13 14:47:41 -0300

    (refactor) Add fetchUsers method in ChallengeView extension

--- InterChallenge/View/Extensions/Delegates/ChallengeViewDelegate.swift
+++ InterChallenge/View/Extensions/Delegates/ChallengeViewDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 extension ChallengeViewController: UserTableViewCellDelegate {
+    
     func didTapAlbums(with userId: Int, by name: String) {
         let userIdAndName = (id: userId, name: name)
         performSegue(withIdentifier: "challengeToAlbum", sender: userIdAndName)
@@ -15,4 +16,21 @@ extension ChallengeViewController: UserTableViewCellDelegate {
         let userIdAndName = (id: userId, name: name)
         performSegue(withIdentifier: "challengeToPost", sender: userIdAndName)
     }
+    
+    //    func setUsers (users: [User]){
+    //        self.users = users
+    //        }
+    //
+    //    func fetchUsers(){
+    //        NetworkManager.shared.getUsers(completed: { [weak self] response in
+    //            switch response {
+    //            case .success(let result):
+    //                self?.setUsers(users: result)
+    //            case .failure(let error):
+    //                print("Error during JSON serialization: \(error.localizedDescription). \(error.rawValue)")
+    //                self!.willDisplayAnErrorHandlerMessage()
+    //            }
+    //        })
+    //    }
+    //    }
 }
