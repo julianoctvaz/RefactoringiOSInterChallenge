028affe84bf4f3e86476cc8ebeb71170ea9f8e6b
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-10-13 09:34:05 -0300

    (build) Implements ChallengeViewModelDelegate base to fetch users data

---
+++ InterChallenge/Extensions/ChallengeViewExtesion.swift
@@ -0,0 +1,28 @@
+//
+//  ChallengeViewExtesion.swift
+//  InterChallenge
+//
+//  Created by Juliano Vaz on 13/10/21.
+//
+
+import Foundation
+
+//extension ChallengeViewController: ChallengeViewModelDelegate {
+//    func setUsers (users: [User]){
+//        self.users = users
+//        }
+//
+//    func fetchUsers(){
+//        NetworkManager.shared.getUsers(completed: { [weak self] response in
+//            switch response {
+//            case .success(let result):
+//                self?.setUsers(users: result)
+//            case .failure(let error):
+//                print("Error during JSON serialization: \(error.localizedDescription). \(error.rawValue)")
+//                self!.willDisplayAnErrorHandlerMessage()
+//            }
+//        })
+//    }
+//    }
+
+
