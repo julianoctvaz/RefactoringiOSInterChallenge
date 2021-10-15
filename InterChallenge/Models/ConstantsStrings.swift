63284610717c6094cfe42653146ed8c7d5cae829
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-10-15 02:25:51 -0300

    (build) Deletes userTableViewCell and improves enum name

---
+++ InterChallenge/Models/ConstantsStrings.swift
@@ -0,0 +1,16 @@
+//
+//  ConstantsStrings.swift
+//  InterChallenge
+//
+//  Created by Juliano Vaz on 14/10/21.
+//
+
+import Foundation
+
+enum ConstantsStrings {
+    static let challengeButtonBackTitle = "Desafio"
+    static let photosButtonBackTitle  = "Fotos"
+    static let albumsButtonTitle = "ALBUNS"
+    static let postsButtonTitle = "POSTAGENS"
+    
+}
