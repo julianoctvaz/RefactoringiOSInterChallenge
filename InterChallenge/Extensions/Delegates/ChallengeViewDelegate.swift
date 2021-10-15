86a292d42453b5df404ae1c800663cc521616a83
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-10-15 07:16:43 -0300

    (build) Rearrange folders and files

--- InterChallenge/Extensions/Delegates/ChallengeViewDelegate.swift
+++ InterChallenge/Extensions/Delegates/ChallengeViewDelegate.swift
@@ -17,20 +17,4 @@ extension ChallengeViewController: UserTableViewCellDelegate {
         performSegue(withIdentifier: "challengeToPost", sender: userIdAndName)
     }
     
-    //    func setUsers (users: [User]){
-    //        self.users = users
-    //        }
-    //
-    //    func fetchUsers(){
-    //        NetworkManager.shared.getUsers(completed: { [weak self] response in
-    //            switch response {
-    //            case .success(let result):
-    //                self?.setUsers(users: result)
-    //            case .failure(let error):
-    //                print("Error during JSON serialization: \(error.localizedDescription). \(error.rawValue)")
-    //                self!.willDisplayAnErrorHandlerMessage()
-    //            }
-    //        })
-    //    }
-    //    }
 }
